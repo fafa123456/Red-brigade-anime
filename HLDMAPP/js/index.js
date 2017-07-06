e671d757c63502ddd093d493b9ae754a3e18c1fc
皮皮虾 <you@example.com>
2017-07-06 10:01:18 +0800

    第一次提交

---
+++ HLDMAPP/js/index.js
@@ -0,0 +1,6 @@
+/*
+ * @Author: zhengwei
+ * @Date:   2016-11-21 09:31:23
+ * @Last Modified by: TinyMark
+ * @Last Modified time: 2017-06-25 15:59:09
+ */
---
+++ js/index.js
@@ -0,0 +1,69 @@
+$.ajax({
+    url: " http://192.168.112.112:8888/api/getlunbo",
+    type: "get",
+    success: function (data) {
+        console.log(data);
+        var str = template("Car_tmp", { list: data });
+        // console.log(str);
+        $(".carousel-inner").html(str);
+        $($(".carousel-inner .item")[0]).addClass("active")
+    }
+});
+
+
+
+
+$.ajax({
+    url: "http://192.168.112.112:8888/api/gethometab/1",
+    type: "get",
+    success: function (data) {
+        var Tabstr = template("tab_Htpl", { lib: data });
+        // console.log(Tabstr);
+        $("#home ul").html(Tabstr);
+
+    }
+});
+
+$(function () {
+    var dom;
+    var lis = document.querySelectorAll("#lis-index li");
+    for (var i = 0; i < lis.length; i++) {
+        lis[i].index = i;
+        lis[i].onclick = function () {
+            var x = this.index + 1;
+            $.ajax({
+                url: "http://192.168.112.112:8888/api/gethometab/" + x,
+                type: "get",
+                success: function (data) {
+                    var Tabstr = template("tab_Htpl", { lib: data });
+                    if (x == 1) {
+                        dom = $("#home ul");
+                    } else if (x == 2) {
+                        dom = $("#profile ul");
+                    } else if (x == 3) {
+                        dom = $("#messages ul");
+                    } else {
+                        dom = $("#settings ul");
+                    }
+                    dom.html(Tabstr);
+                }
+            });
+        }
+    }
+    $('#myTabs a').click(function (e) {
+        e.preventDefault()
+        $(this).tab('show')
+    });
+
+
+    // 点击 左上角的 按钮显示侧边栏
+    $(".left-meun")
+    $(".meun").on("click", function () {
+        $(".left-meun").css("display", "block");
+        $(".bingbox").css("transform", "translateX(200px)");
+    })
+    
+})
+
+
+ 
